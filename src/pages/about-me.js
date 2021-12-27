365ade9bd644a826c8e27e72b362bdf6f387e50e
Drina_Rincon <drina.rincon@bitlogic.io>
2021-12-27 15:43:17 -0300

    testing

---
+++ src/pages/about-me.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import LayoutBasic from '../layouts/LayoutBasic';
+
+export default function AboutMe() {
+    return (
+        <LayoutBasic>
+            <h2>Estamos en AboutMe</h2>
+        </LayoutBasic>
+    )
+}
