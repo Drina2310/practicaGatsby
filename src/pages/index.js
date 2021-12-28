34766bbc5e609e99dc16077b620718c396393f5e
Drina_Rincon <drina.rincon@bitlogic.io>
2021-12-28 14:45:56 -0300

    añadiendo sass al proyecto

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,14 @@ import * as React from "react";
 import { Button } from 'react-bootstrap';
 import LayoutBasic from '../layouts/LayoutBasic';
 import SeoC from '../components/SeoC';
+import './index.scss';
 
 export default function IndexPage() {
   return (
 
     <LayoutBasic>
       <SeoC title="Home" />
-      <h2>Estamos en la home</h2>
+      <h2 className="title">Estamos en la home</h2>
       <Button variant="danger">Danger</Button>
     </LayoutBasic>
 
