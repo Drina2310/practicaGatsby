23c49ce004be54d16492c5ff72c6bcb10b547328
Drina_Rincon <drina.rincon@bitlogic.io>
2021-12-29 12:18:48 -0300

    crear componente q nos devuelve la imagen

---
+++ src/components/Image_OLD.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
+
+export default function Image() {
+    const data = useStaticQuery(graphql`
+        query {
+            image: file(relativePath: { eq: "gatsby-astronaut.png"}) {
+                sharp: childImageSharp {
+                    fluid {
+                        src
+                    }
+                }
+            }
+        }
+    `)
+
+    console.log(data);
+    return <img src={data.image.sharp.fluid.src} alt="" />
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import * as React from "react";
 import { Button } from 'react-bootstrap';
 import LayoutBasic from '../layouts/LayoutBasic';
 import SeoC from '../components/SeoC';
+import Imagen from '../components/Image';
 import './index.scss';
 
 export default function IndexPage() {
@@ -11,7 +12,7 @@ export default function IndexPage() {
       <SeoC title="Home" />
       <h2 className="title">Estamos en la home</h2>
       <Button variant="danger">Danger</Button>
+      <Imagen />
     </LayoutBasic>
-
   )
 }
